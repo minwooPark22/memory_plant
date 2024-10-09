2681bfa02754088c766257dd3c55e90aa1736786
Lee <cindy201156@gmail.com>
2024-10-10 00:43:34 +0900

    setting page(/w setting widget & blue box)

--- memory_plant_application/lib/screens/setting_page.dart
+++ memory_plant_application/lib/screens/setting_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:memory_plant_application/widgets/setting_list.dart';    //original widget
+import 'package:memory_plant_application/widgets/blue_box.dart';   //blue box widget
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -17,16 +19,17 @@ class SettingPage extends StatelessWidget {
           },
         ),
 
-          title: Text(isKorean ? "⚙️️설정" : "⚙️Settings"),
-          actions: [
-
+          title: Text(isKorean ? "⚙️️설정" : "⚙️Settings"), //일단 임티로 대체
+        ),
+        body: Stack(
+          children: [
+            const BlueBox(),
+            Padding(
+              padding: const EdgeInsets.only(top: 32.0),
+              child: const SettingsList(),
+            ),
           ],
         ),
-      body: Container(
-        color: Colors.lightBlue[400], // 배경 색상
-        child: Center( //알단 노
-        ),
-      ),
-    );
+      );
+    }
   }
-}
