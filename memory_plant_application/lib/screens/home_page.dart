3c1d107bb807f2c8440a63f96d406bdf45c45dd1
Kim2gom <150894622+Kim2gom@users.noreply.github.com>
2024-10-09 18:43:29 +0900

    저장소 scaffold까지 만듦

---
+++ memory_plant_application/lib/screens/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:memory_plant_application/screens/add_page.dart';
+
+class home_Page extends StatefulWidget {
+  const home_Page({super.key});
+
+  @override
+  State<home_Page> createState() => _home_PageState();
+}
+
+class _home_PageState extends State<home_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
