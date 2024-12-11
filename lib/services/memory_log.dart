66434f0f94400861fd2b704184b80ac837c7fc13
Lee <cindy201156@gmail.com>
2024-12-11 21:02:04 +0900

    memory 끗

--- lib/services/memory_log.dart
+++ lib/services/memory_log.dart
@@ -1,18 +1,20 @@
 class MemoryLog {
+  String? memoryId;
   String? title;
   String? contents;
   String? timestamp;
   bool? isUser;
-  MemoryLog({this.title, this.contents, this.timestamp, this.isUser});
-  // isUser 추가함
+  MemoryLog({this.memoryId, this.title, this.contents, this.timestamp, this.isUser});
 
-  MemoryLog.fromJson(Map<String, dynamic> json) {
+
+  MemoryLog.fromJson(Map<String, dynamic> json, String docId) {
+    memoryId = docId; // Firestore 문서 ID
     title = json['title'];
     contents = json['contents'];
     timestamp = json['timestamp'];
     isUser = json['isUser'];
   }
-
+/*
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['title'] = title;
@@ -22,5 +24,16 @@ class MemoryLog {
     return data;
   }
 }
+ */
 
+  // JSON으로 변환
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'contents': contents,
+      'timestamp': timestamp,
+      'isUser': isUser,
+    };
+  }
+}
 
--- lib/services/memory_log_service.dart
+++ lib/services/memory_log_service.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+/*import 'dart:io';
 import 'dart:convert';
 import 'package:memory_plant_application/services/memory_log.dart';
 import 'package:path_provider/path_provider.dart';
@@ -41,5 +41,58 @@ class MemoryLogService {
     List<MemoryLog> memories = await loadMemoryLogs();
     memories.add(newMemory);
     await saveMemoryLogs(memories);
+  }
+}
+*/
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:memory_plant_application/services/memory_log.dart';
+
+class MemoryLogService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // 메모리 저장
+  Future<void> saveMemoryLog(MemoryLog memory) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final memoriesCollection = _firestore.collection('memories');
+      await memoriesCollection.add(memory.toJson()); // 새로운 메모리를 Firestore에 추가
+    }
+  }
+
+  // 메모리 불러오기
+  Future<List<MemoryLog>> loadMemoryLogs() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final memoriesCollection = _firestore
+          .collection('memories')
+          .orderBy('timestamp', descending: true);
+      final querySnapshot = await memoriesCollection.get();
+
+      return querySnapshot.docs.map((doc) {
+        final data = doc.data();
+        return MemoryLog.fromJson(data, doc.id); // 문서 데이터와 ID를 함께 전달
+      }).toList();
+    } else {
+      return [];
+    }
+  }
+
+  // 메모리 삭제
+  Future<void> deleteMemory(String memoryId) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final memoriesCollection = _firestore.collection('memories');
+      await memoriesCollection.doc(memoryId).delete();
+    }
+  }
+
+  // 메모리 업데이트
+  Future<void> updateMemory(String memoryId, MemoryLog updatedMemory) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final memoriesCollection = _firestore.collection('memories');
+      await memoriesCollection.doc(memoryId).update(updatedMemory.toJson());
+    }
   }
 }
